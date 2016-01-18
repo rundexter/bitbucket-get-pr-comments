137b7d1431f1864b1c39794ef7264770e74488be
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-18 14:55:46 +0200

    fix Oauth

--- index.js
+++ index.js
@@ -5,73 +5,29 @@ var request = require('request').defaults({
     baseUrl: 'https://api.bitbucket.org/2.0/'
 });
 
-var globalPickResult = {
-    '-': {
-        keyName: 'values',
-        fields: {
-            'id': 'id',
-            'created_on': 'created_on',
-            'links_html': 'links.html.href',
-            'content_raw': 'content.raw',
-            'user_username ': 'user.username',
-            'user_links_self': 'user.links.self.href'
+var pickInputs = {
+        'id': { key: 'id', validate: { req: true } },
+        'owner': { key: 'owner', validate: { req: true } },
+        'repo_slug': { key: 'repo_slug', validate: { req: true } },
+        'branchortag': 'branchortag',
+        'include': 'include',
+        'exclude': 'exclude'
+    },
+    pickOutputs = {
+        '-': {
+            key: 'values',
+            fields: {
+                'id': 'id',
+                'created_on': 'created_on',
+                'links_html': 'links.html.href',
+                'content_raw': 'content.raw',
+                'user_username ': 'user.username',
+                'user_links_self': 'user.links.self.href'
+            }
         }
-    }
-};
+    };
 
 module.exports = {
-
-    authParams: function (dexter) {
-        var auth = {},
-            username = dexter.environment('bitbucket_username'),
-            password = dexter.environment('bitbucket_password');
-
-        if (username && password) {
-
-            auth.user = username;
-            auth.pass = password;
-        }
-
-        return _.isEmpty(auth)? false : auth;
-    },
-
-    processResult: function (error, responce, body) {
-
-        if (error)
-
-            this.fail(error);
-
-        else if (responce && !body)
-
-            this.fail(responce.statusCode + ': Something is happened');
-
-        else if (responce && body.error)
-
-            this.fail(responce.statusCode + ': ' + JSON.stringify(body.error));
-
-        else
-
-            this.complete(util.pickResult(body, globalPickResult));
-
-    },
-
-    checkCorrectParams: function (auth, step) {
-        var result = true;
-
-        if (!auth) {
-
-            result = false;
-            this.fail('A [bitbucket_username, bitbucket_password] environment need for this module.');
-        }
-
-        if (!step.input('owner').first() || !step.input('repo_slug').first() || !step.input('id').first()) {
-
-            result = false;
-            this.fail('A [owner, repo_slug, revision] inputs need for this module.');
-        }
-
-        return result;
-    },
 
     /**
      * The main entry point for the Dexter module
@@ -80,24 +36,28 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
+        var credentials = dexter.provider('bitbucket').credentials(),
+            inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs);
 
-        var auth = this.authParams(dexter);
         // check params.
-        if (!this.checkCorrectParams(auth, step)) return;
+        if (validateErrors)
+            return this.fail(validateErrors);
 
-        var formData = {},
-            owner = step.input('owner').first().trim(),
-            repoSlug = step.input('repo_slug').first().trim(),
-            pullRequestId = step.input('id').first().trim();
-
-        if (step.input('state').first())
-            formData.state = step.input('state').first();
-
-        var uriLink = 'repositories/' + owner + '/' + repoSlug + '/pullrequests/' + pullRequestId + '/comments';
+        var uriLink = 'repositories/' + inputs.owner + '/' + inputs.repo_slug + '/pullrequests/' + inputs.id + '/comments';
         //send API request
-        request.get({url: uriLink, qs: formData, auth: auth, json: true}, function (error, responce, body) {
-
-            this.processResult(error, responce, body);
+        request.get({
+            uri: uriLink,
+            qs: _.pick(inputs, 'state'),
+            oauth: credentials,
+            json: true
+        }, function (error, response, body) {
+            if (error || (body && body.error))
+                this.fail(error || body.error);
+            else if (typeof body === 'string')
+                this.fail('Status code: ' + response.statusCode);
+            else
+                this.complete(util.pickOutputs(body, pickOutputs) || {});
         }.bind(this));
     }
 };
